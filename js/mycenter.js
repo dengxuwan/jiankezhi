06f737b64d0c475b05d8ad4d6b4e02a5801f997f
anyoneortwo <424057214@qq.com>
2018-06-02 14:55:08 +0800

    modified:   css/index.css 	modified:   js/mycenter.js

--- js/mycenter.js
+++ js/mycenter.js
@@ -4,22 +4,29 @@ template.defaults.imports.dateFormat = function(date) {
 
 var centerObject = {};
 var curWallet;
+
 function getWallectInfo() {
+
 	window.postMessage({
 		"target": "contentscript",
 		"data": {},
 		"method": "getAccount",
 	}, "*");
 	window.addEventListener('message', function(e) {
-		if (e.data && e.data.data) {
+		console.log(e, 111111111111111111)
+		if (e.data && e.data.data && e.data.data.account) {
 			if (e.data.data.account) {
 				curWallet = e.data.data.account;
 				queryCenterInfo();
 			}
+		} else {
+			$(".loading").hide();
+			alert("必须安装星云钱包插件！");
 		}
 	});
 }
-function queryCenterInfo(){
+
+function queryCenterInfo() {
 	//获取个人信息列表;
 	query(curWallet, config.personal, "", function(resp) {
 		console.log(resp, "获取个人信息返回数据");
@@ -40,7 +47,8 @@ function queryCenterInfo(){
 		$(".loading").hide();
 	});
 }
-function toIndexSearch(){
+
+function toIndexSearch() {
 	var keyword = $("#keyword").val();
-	window.location.href="index.html?keyword="+keyword;
+	window.location.href = "index.html?keyword=" + keyword;
 }
